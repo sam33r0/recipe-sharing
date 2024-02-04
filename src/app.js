965fc5251d16d4882964d99c192bdd7036b36c65
sam33r0 <srivastavasameer2002@gmail.com>
2024-02-04 18:41:00 +0530

    fixed bugs and made utilities

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 const app= express();
 
 app.use(cors({
@@ -7,8 +8,8 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.json({ limit: "32kb" }))
+app.use(express.urlencoded({ extended: true, limit: "32kb" }))
 app.use(express.static("public"))
 app.use(cookieParser());
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-
-
 const router = Router();
 
+router.route('/').get((req,res)=>{
+    res.send("<h1>hello</h1>");
+})
 
 export default router;
