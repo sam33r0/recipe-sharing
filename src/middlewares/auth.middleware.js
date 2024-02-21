37fc23d8534b89021fb385f152f036dc1ead524c
sam33r0 <srivastavasameer2002@gmail.com>
2024-02-21 11:54:11 +0530

    minor changes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,21 +6,19 @@ import { User } from "../models/user.model.js";
 const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-        if (!token) 
-        {
+        if (!token) {
             throw new ApiError(401, "Unauthorized request");
         }
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);  
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
-        if (!user) 
-        {
+        if (!user) {
             throw new ApiError(401, "Invalid access token");
         }
         req.user = user;
         next();
     }
-    catch(error){
-        throw new ApiError(401 , error?.message || "Invalid access token!")
+    catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token!")
     }
 })
-export {verifyJWT}
+export { verifyJWT }
