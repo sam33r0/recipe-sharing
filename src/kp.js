4335d4d02669db149ff24a09bbe118687a2a40ea
sam33r0 <srivastavasameer2002@gmail.com>
2024-03-09 14:54:24 +0530

    cors change test

--- src/kp.js
+++ src/kp.js
@@ -8,8 +8,9 @@ import  recipeRoutes  from "./routes/recipe.routes.js";
 const app= express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: true,
     credentials: true,
+    
 }));
 
 app.use(express.json({ limit: "32kb" }))
