a739c4ecc619718d3f497d475082c164415257e0
sam33r0 <srivastavasameer2002@gmail.com>
2024-03-14 13:52:16 +0530

    live testing

--- src/kp.js
+++ src/kp.js
@@ -17,7 +17,9 @@ app.use(express.json({ limit: "32kb" }))
 app.use(express.urlencoded({ extended: true, limit: "32kb" }))
 app.use(express.static("public"))
 app.use(cookieParser());
-
+app.get('/',(req,res)=>{
+    res.json({"message":"It is live"})
+});
 app.use("/reciapi/v1/users",userRoutes);
 app.use("/reciapi/v1/recipe", recipeRoutes)
 export { app };
