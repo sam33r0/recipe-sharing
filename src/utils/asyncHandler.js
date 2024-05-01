0b5e4d2f73ee05946e9452c1bb34575393e72daa
sam33r0 <srivastavasameer2002@gmail.com>
2024-05-01 23:28:25 +0530

    updte

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -4,14 +4,3 @@ const asyncHandler= (requestHandler)=>{
     }
 }
 export {asyncHandler}
-
-// const asyncHandler = (fn) => async(req,res,next)=> {
-//     try {
-//         await fn(req,res,next);
-//     } catch (err) {
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message: err.message
-//         })
-//     }
-// }
